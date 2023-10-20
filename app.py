a9bfd8213e35f219e4ae10108665faaaacf44bde
Kieran Matheson <kieran.w.c.matheson@gmail.com>
2023-10-20 14:32:40 +0100

    dependancy update

--- app.py
+++ app.py
@@ -11,4 +11,5 @@ if __name__ == '__main__':
     app.run(
         host="0.0.0.0",
         debug=True,
+        
     )
--- tests/test_app.py
+++ tests/test_app.py
@@ -1,4 +1,14 @@
-# def test_get_home(web_client):
-#     response = web_client.get("/")
-#     assert response.status_code == 200
-#     assert response.data.decode("utf-8") == "Hello, world!"
+from playwright.sync_api import Page, expect
+
+def test_get_home(web_client):
+    response = web_client.get("/")
+    assert response.status_code == 200
+
+
+
+# will no work without SSL certificate
+# def test_has_title(page: Page):
+#     page.goto("https://kmatheson-portfolio.xf.mkrs.link")
+#     h1_tags = page.locator("h1")
+#     # Expect a title "to contain" a substring.
+#     expect(h1_tags).to_have_text("Kieran Matheson")
